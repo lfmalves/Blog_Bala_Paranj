56fb928efa59cab123f3b46cd6b3e9c6c5a035c7
lfmalves <luis.projensino@gmail.com>
2020-01-31 14:10:27 -0300

    feat: comments controller

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_131_113_511) do
+ActiveRecord::Schema.define(version: 20_200_131_170_416) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'description'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'description'
+    t.integer 'article_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['article_id'], name: 'index_comments_on_article_id'
+  end
+
+  add_foreign_key 'comments', 'articles'
 end
