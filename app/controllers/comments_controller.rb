3be6a3e841545e76e8b859bb30b89ccffdf6082e
lfmalves <luis.projensino@gmail.com>
2020-01-31 14:41:06 -0300

    feat: comments on show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    permitted_columns = params[:comment].permit(:commenter, :description)
+    @comment = @article.comments.create(permitted_columns)
+
+    redirect_to article_path(@article)
+  end
 end
